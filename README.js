719fb3d53d59d926d37c3a90daf7fdd8b50df45c
typicalwinner <nzsjsis@gmail.com>
2022-10-07 20:42:14 -0700

    Delete README.js

--- README.js
+++
@@ -1,2 +0,0 @@
-this is basically my storage of scripts 
-// javascript:fetch("https://raw.githubusercontent.com/typicalwinner/scripts/main/function.js").then(data=>{data.text().then(text=>{eval(text)})});
